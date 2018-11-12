0ce40a0f1547c604f2907be173e0dadeea43ec01
TeoGJ <teogonzalez84@gmail.com>
2018-11-12 15:14:25 +0100

    2nd commit

--- definedsprocess.R
+++ definedsprocess.R
@@ -1,3 +1,24 @@
 
 
 #### first commit
+
+setwd("C:/Users/User/Desktop/DataAnalytics/41DefineDSProcess")
+
+
+#### Loading and exploring dataset
+
+df<- read.table("household_power_consumption.txt", sep = ";", dec = ".", header = TRUE)
+
+df[1:6,]
+head(df)
+
+
+df <-cbind(df,paste(df$Date,df$Time), stringsAsFactors=FALSE)
+colnames(df)[10] <-"DateTime"
+df <- df[,c(ncol(df), 1:(ncol(df)-1))]
+head(df)
+
+
+df$DateTime <- strptime(df$DateTime, "%d/%m/%Y %H:%M:%S")
+df$Date <- as.Date(df$Date, "%d/%m/%Y")
+str(df)
